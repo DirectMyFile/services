c9f58d27876f76979e6b01821768639bccde48c8
Kenneth Endfinger <kaendfinger@gmail.com>
2014-10-06 14:28:24 -0400

    Add Global Listeners

--- lib/src/api/events.dart
+++ lib/src/api/events.dart
@@ -7,27 +7,28 @@ class EventEndpoint {
   Map<String, List<WebSocketSession>> events = {};
   Map<WebSocketSession, String> tokened = {};
   Map<String, int> eventCounts = {};
-  
+  List<WebSocketSession> globalListeners = [];
+
   EventEndpoint() {
     eventEndpoint = this;
   }
-  
+
   @OnOpen()
   void onOpen(WebSocketSession session) {
     sendMessage(session, {
       "type": "ready"
     });
   }
-  
+
   void sendMessage(WebSocketSession session, Map message) {
     var json = Convert.JSON.encode(message);
     session.connection.add(json);
   }
-  
+
   @OnMessage()
   void onMessage(String message, WebSocketSession session) {
     var json = Convert.JSON.decode(message);
-    
+
     if (json['type'] == null) {
       sendMessage(session, {
         "type": "error",
@@ -36,12 +37,12 @@ class EventEndpoint {
       });
       return;
     }
-    
+
     String type = json['type'];
-    
+
     if (type == "token") {
       var token = json['token'];
-      
+
       if (token == null) {
         sendMessage(session, {
           "type": "error",
@@ -50,7 +51,7 @@ class EventEndpoint {
         });
         return;
       }
-      
+
       if (!tokens.contains(token)) {
         sendMessage(session, {
           "type": "error",
@@ -59,12 +60,12 @@ class EventEndpoint {
         });
         return;
       }
-      
+
       tokened[session] = token;
-      
+
       return;
     }
-    
+
     if (!tokened.containsKey(session)) {
       sendMessage(session, {
         "type": "error",
@@ -73,7 +74,7 @@ class EventEndpoint {
       });
       return;
     }
-    
+
     if (!tokens.contains(tokened[session])) {
       sendMessage(session, {
         "type": "error",
@@ -82,10 +83,10 @@ class EventEndpoint {
       });
       return;
     }
-    
+
     if (type == "register") {
       var event = json['event'];
-      
+
       if (event == null) {
         sendMessage(session, {
           "type": "error",
@@ -94,10 +95,10 @@ class EventEndpoint {
         });
         return;
       }
-      
-      var list = events.putIfAbsent(event, () => []);
-      
-      if (list.contains(session)) {
+
+      var list = event == "*" ? globalListeners : events.putIfAbsent(event, () => []);
+
+      if (list.contains(session) || globalListeners.contains(session)) {
         sendMessage(session, {
           "type": "error",
           "error": "event.already.registered",
@@ -105,16 +106,16 @@ class EventEndpoint {
         });
         return;
       }
-      
+
       list.add(session);
-      
+
       sendMessage(session, {
         "type": "registered",
         "event": event
       });
     } else if (type == "unregister") {
       var event = json['event'];
-      
+
       if (event == null) {
         sendMessage(session, {
           "type": "error",
@@ -123,9 +124,9 @@ class EventEndpoint {
         });
         return;
       }
-      
-      var list = events.putIfAbsent(event, () => []);
-      
+
+      var list = event == "*" ? globalListeners : events.putIfAbsent(event, () => []);
+
       if (!list.contains(session)) {
         sendMessage(session, {
           "type": "error",
@@ -134,16 +135,16 @@ class EventEndpoint {
         });
         return;
       }
-      
+
       list.remove(session);
-      
+
       sendMessage(session, {
         "type": "unregistered",
         "event": event
       });
     } else if (type == "emit") {
       var event = json['event'];
-      
+
       if (event == null) {
         sendMessage(session, {
           "type": "error",
@@ -152,11 +153,11 @@ class EventEndpoint {
         });
         return;
       }
-      
+
       var data = json['data'];
-      
+
       if (data == null) data = {};
-      
+
       emit(event, data);
     } else {
       sendMessage(session, {
@@ -166,30 +167,39 @@ class EventEndpoint {
       });
     }
   }
-  
+
   void emit(String eventName, Map data) {
     if (!eventCounts.containsKey(eventName)) {
       eventCounts[eventName] = 0;
     }
-    
+
     eventCounts[eventName] = eventCounts[eventName] + 1;
-    
+
     if (!events.containsKey(eventName)) return;
+
+    var msg = {
+      "type": "event",
+      "event": eventName,
+      "data": data
+    };
+
     for (var session in events[eventName]) {
-      sendMessage(session, {
-        "type": "event",
-        "event": eventName,
-        "data": data
-      });
+      sendMessage(session, msg);
+    }
+    
+    for (var session in globalListeners) {
+      sendMessage(session, msg);
     }
   }
-  
+
   @OnClose()
   void onClose(WebSocketSession session) {
     for (var list in events.values) {
       list.remove(session);
     }
     
+    globalListeners.remove(session);
+
     tokened.remove(session);
   }
 }
@@ -200,17 +210,17 @@ class EventService {
   stats() {
     var endpoint = eventEndpoint;
     var listeners = {};
-    
+
     for (var event in endpoint.events.keys) {
       listeners[event] = endpoint.events[event].length;
     }
-    
+
     var eventz = {};
-    
+
     for (var event in endpoint.eventCounts.keys) {
       eventz[event] = endpoint.eventCounts[event];
     }
-    
+
     return {
       "listeners": listeners,
       "events": eventz
