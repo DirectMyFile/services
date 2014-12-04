7d42d8d61d3a47c5ade8b42c25bf58ff7a4a241b
Kenneth Endfinger <kaendfinger@gmail.com>
2014-12-03 22:24:56 -0500

    improve token generation

--- lib/src/common/tokens.dart
+++ lib/src/common/tokens.dart
@@ -30,6 +30,7 @@ void TokenPlugin(Manager manager) {
         "error": "token.permission.missing",
         "message": "The token that was provided does not have the required permissions to use this API."
       });
+
     } else {
       return route(pathSegments, injector, request);
     }
@@ -93,13 +94,15 @@ bool hasPermission(String token, String perm) {
   return false;
 }
 
-String generateToken({int length: 30}) {
+String generateToken({int length: 50}) {
+  Random r = new Random();
   var buffer = new StringBuffer();
   for (int i = 1; i <= length; i++) {
-    if (random.nextBool()) {
-      buffer.write(alphabet[random.nextInt(alphabet.length)]);
+    if (r.nextBool()) {
+      String letter = alphabet[random.nextInt(alphabet.length)];
+      buffer.write(r.nextBool() ? letter.toLowerCase() : letter);
     } else {
-      buffer.write(numbers[random.nextInt(numbers.length)]);
+      buffer.write(numbers[r.nextInt(numbers.length)]);
     }
   }
   return buffer.toString();
@@ -139,4 +142,4 @@ class TokenManager {
 }
 
 const List<String> alphabet = const ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const List<String> numbers = const [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const List<int> numbers = const [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
