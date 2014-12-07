d2a641392bbc5a37884dbfbc8cd75cbd60ff2e88
Logan Gorence <loganjohngorence@gmail.com>
2014-12-06 21:35:50 -0800

    Add Sam quote.

--- lib/src/api/quotes.dart
+++ lib/src/api/quotes.dart
@@ -1,6 +1,6 @@
 part of directcode.services.api;
 
-const List<String> _QUOTES = const ["I'm going to fstab this fstab. -Logan"];
+const List<String> _QUOTES = const ["I'm going to fstab this fstab. -Logan", "if (username == \"samrg472\" && password == \"12345\") // Such secure authentication -samrg472"];
 
 @Route("/quote")
 quote() {
