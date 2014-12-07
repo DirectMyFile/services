d608bf6f00ca32598c2a65045a9decdbfe362330
Kenneth Endfinger <kaendfinger@gmail.com>
2014-12-06 22:10:03 -0500

    again wtf

--- server/lib/services.dart
+++ server/lib/services.dart
@@ -40,4 +40,4 @@ allowCORS() {
   }
 }
 
-_createCorsHeader() => {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Content-Type, X-DirectCode-Token"};
+_createCorsHeader() => {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "CONTENT-TYPE, X-DIRECTCODE-TOKEn"};
