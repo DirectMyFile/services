70b4d9b23fa028a35cab23a401fcc9f4d8326a56
Kenneth Endfinger <kaendfinger@gmail.com>
2015-01-14 23:39:19 -0500

    Work on the stats page for events.

--- www/web/events/stats.dart
+++ www/web/events/stats.dart
@@ -1,26 +1,23 @@
 import "dart:html";
 import "dart:convert";
-import "package:polymer/polymer.dart";
 
 void main() {
-  initPolymer();
-}
+  HttpRequest.getString("/api/events/stats").then((value) {
+    var json = JSON.decode(value);
+    var listeners = json["listeners"].keys.map((it) {
+      return new Listener()..name = it..count = json["listeners"][it];
+    }).toList();
 
-@CustomTag("services-event-stats")
-class ServicesEventStats extends PolymerElement {
-  @observable List<Listener> listeners = [];
-  
-  ServicesEventStats.created() : super.created();
-  
-  @override
-  void attached() {
-    HttpRequest.getString("/api/events/stats").then((value) {
-      var json = JSON.decode(value);
-      listeners = json["listeners"].keys.map((it) {
-        return new Listener()..name = it..count = json["listeners"][it];
-      }).toList();
-    });
-  }
+    var $l = querySelector("#rows-listeners");
+    for (var listener in listeners) {
+      $l.appendHtml("""
+      <tr>
+        <td>${listener.name}</td>
+        <td>${listener.count}</td>
+      </tr>
+      """);
+    }
+  });
 }
 
 class Listener {
