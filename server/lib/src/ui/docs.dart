81d9f62b7877a7dd19d9257eab196ab61c3a0e97
Kenneth Endfinger <kaendfinger@gmail.com>
2015-01-16 19:26:27 +0000

    Fix

--- server/lib/src/ui/docs.dart
+++ server/lib/src/ui/docs.dart
@@ -3,10 +3,10 @@ part of directcode.services.ui;
 @Route("/docs/:page.md")
 @Markdown()
 docs(String page) {
-  var file = new File("docs/${page}.md");
+  var file = new File("../docs/${page}.md");
   
   if (!file.existsSync()) {
-    return new ErrorResponse(404, "Page Not Found");
+    throw new ErrorResponse(404, "Page Not Found");
   }
   
   return file.readAsString();
