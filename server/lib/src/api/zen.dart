33748309a518dcaa374e5bc7fa01a4da67cf6683
Kenneth Endfinger <kaendfinger@gmail.com>
2015-01-16 14:02:22 +0000

    Reformating

--- server/lib/src/api/zen.dart
+++ server/lib/src/api/zen.dart
@@ -1,6 +1,18 @@
 part of directcode.services.api;
 
-const List<String> _ZENS = const ["Half measures are as bad as nothing at all.", "Keep it logically awesome.", "Don't be evil.", "Be fun.", "Don't be afraid to fail.", "Practicality beats purity.", "Make mistakes, and learn from them.", "Do not be afraid to rewrite something, if it is for a better cause.", "Innovation is just as good as invention.", "Shoot for the best and you get the rest.", "Break it."];
+const List<String> _ZENS = const [
+  "Half measures are as bad as nothing at all.",
+  "Keep it logically awesome.",
+  "Don't be evil.",
+  "Be fun.",
+  "Don't be afraid to fail.",
+  "Practicality beats purity.",
+  "Make mistakes, and learn from them.",
+  "Do not be afraid to rewrite something, if it is for a better cause.",
+  "Innovation is just as good as invention.",
+  "Shoot for the best and you get the rest.",
+  "Break it."
+];
 
 @Route("/zen")
 zen() {
